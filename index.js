c0af540e52cd555cd6a69924cc8269cad19ce869
chuks <chuksisonchain@gmail.com>
2025-11-27 03:49:37 +0100

    upgraded background worker for my endpoint

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-
 const axiosModule = require("axios");
-const delay=5000
+const delay = 4000;
 
-const _startBackgroundWorker = async ()=>{
-       const api_url="https://mile83autos-api-backend.onrender.com/api/v1/checkServerHealth";
-       try{
-        const response = await axiosModule.get(api_url);
-        console.log(`Server responded :: ${new Date().toISOString()} `)
-        console.log(`Metric::`,response.data)
-       }
-       catch(e){
-        console.log("Something went wrong, could no tstart cron job")
-       }
-}
+const _startBackgroundWorker = async () => {
+  const api_endpoint_url = [
+    "https://mile83autos-api-backend.onrender.com/api/v1/checkServerHealth",
+    "https://mile83autos-ai-node.onrender.com/Mile83LLMModel/api/v1/isAIServerOnline",
+  ];
 
+  try {
+    api_endpoint_url.map(async (endpointurl, i) => {
+      const response = await axiosModule.get(endpointurl);
+      if (response) {
+        console.log(`Server responded :: ${new Date().toISOString()} `);
+        console.log(`Metric::`, response.data);
+      }
+    });
+  } catch (e) {
+    console.log("Something went wrong, could no start url cron job");
+  }
+};
 
-
-setInterval(_startBackgroundWorker,delay);
- 
+setInterval(_startBackgroundWorker, delay);
